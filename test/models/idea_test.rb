52bd25b7c13de7d422d22b7b04277603f67764e1
maryamaln <maryamaln@gmail.com>
2016-11-19 23:24:39 +0500

    model unit tests

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "Idea should be created without title" do
+  	idea = Idea.new(description: 'Sammy', skills_description: 'Sams', platform: 'lahore',help_needed:'ume',user_id: 1) 
+    assert_not idea.save
+  end
+
+  test "Idea should be created without description" do
+  	idea = Idea.new(title: 'Sammy', skills_description: 'Sams', platform: 'lahore',help_needed:'ume',user_id: 1) 
+    assert_not idea.save
+  end
+
+  test "Idea should be created without skills_description" do
+  	idea = Idea.new(title: 'Sammy', description: 'Sams', platform: 'lahore',help_needed:'ume',user_id: 1) 
+    assert_not idea.save
+  end
+
+  test "Idea should be created without help_needed" do
+  	idea = Idea.new(title: 'Sammy', description: 'Sams', platform: 'lahore',skills_description:'ume',user_id: 1) 
+    assert_not idea.save
+  end
+
+  test "Idea should be created without platform" do
+  	idea = Idea.new(title: 'Sammy', description: 'Sams', description: 'lahore',skills_description:'ume',user_id: 1) 
+    assert_not idea.save
+  end
+
+  test "Idea should be created without user_id" do
+  	idea = Idea.new(title: 'Sammy', description: 'Sams', description: 'lahore',skills_description:'ume',platform: "Pak") 
+    assert_not idea.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "User count in fixtures" do
+    assert_equal 0, User.count
+  end
+
+  test "User should be created without  email" do
+  	user = User.new(first_name: 'Sammy', last_name: 'Sams', address: 'lahore',user_name:'ume') 
+    assert_not user.save
+  end
+
+  test "User should be created without  first_name" do
+  	user = User.new(email: 'ume19@hotmail.com', last_name: 'Sams', address: 'lahore',user_name:'ume') 
+    assert_not user.save
+  end
+
+  test "User should be created without  last_name" do
+  	user = User.new(email: 'ume19@hotmail.com', first_name: 'Sams', address: 'lahore',user_name:'ume') 
+    assert_not user.save
+  end
+
+  test "User should be created without  address" do
+  	user = User.new(first_name: 'Sammy',email: 'ume19@hotmail.com', first_name: 'Sams', user_name:'ume') 
+    assert_not user.save
+  end
+
+  test "User should be created without  user_name" do
+  	user = User.new(first_name: 'Sammy',email: 'ume19@hotmail.com', first_name: 'Sams', address:'Pak') 
+    assert_not user.save
+  end
+  
 end
